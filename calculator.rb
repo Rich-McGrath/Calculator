9f4949508f6076a6e363b434c696b6d2c4d54107
Rich-McGrath <richmcgrath@gmail.com>
2015-09-23 23:08:45 -0400

    Removed code

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,6 @@
 # Go Tea Leaf Calculator
 
-loop do
-  # Check to see if user still wants to use the Calculator.
+loop do # Check to see if user still wants to use the Calculator.
   puts 'Would you like to use Calculator?'
   response = gets.chomp.capitalize
   if response == 'N'
@@ -10,12 +9,15 @@ loop do
   # User is asked to enter in two numnbers.
   puts 'Please enter a number:'
   number1 = gets.chomp.to_i
+
   puts 'Please enter in a second number:'
   number2 = gets.chomp.to_i
+
   # User now selects the operator.
   puts 'Enter in the number for the Operation you want performed'
   puts '1) Add 2) Subtract 3) Multiply 4) Divide'
   operator = gets.chomp
+
   case operator
   when '1' # Addition
     # Mutating the Caller on all case statments
@@ -28,7 +30,7 @@ loop do
     answer = number1 * number2
     operator = ' * '
   when '4' # Division
-    answer = number1 / number2
+    answer = number1.to_f / number2.to_f
     operator = ' / '
   end
   puts "The answer for #{number1} #{operator} #{number2} is: #{answer}"
